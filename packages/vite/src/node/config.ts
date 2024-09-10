db165dccfcdd2a665a30b8c34da93e4068b309f1
IWANABETHATGUY <iwanabethatguy@qq.com>
2024-09-10 15:20:06 +0800

    feat: add experimental.enableNativePlugin (#41)
    2cf0eb2dca47d436f65df5fe81373183c675c3e6 fe71cbc5fb4c0766d024fe1df5b44c15121ee71b
    
    Co-authored-by: IWANABETHATGUY <iwanabethatguy@qq.com>

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -507,6 +507,14 @@ export interface ExperimentalOptions {
    * @default false
    */
   skipSsrTransform?: boolean
+
+  /**
+   * Enable builtin plugin that written by rust, which is faster than js plugin.
+   *
+   * @experimental
+   * @default true
+   */
+  enableNativePlugin?: boolean
 }
 
 export interface LegacyOptions {
@@ -1438,6 +1446,7 @@ export async function resolveConfig(
     experimental: {
       importGlobRestoreExtension: false,
       hmrPartialAccept: false,
+      enableNativePlugin: false,
       ...config.experimental,
     },
     future: config.future,
