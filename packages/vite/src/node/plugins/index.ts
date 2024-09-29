74677f521f8b487ab27e9abeafa30af7082764e0
underfin <likui.underfin@gmail.com>
2024-09-29 22:27:25 +0800

    feat: using oxc transfomer

--- packages/vite/src/node/plugins/index.ts
+++ packages/vite/src/node/plugins/index.ts
@@ -18,7 +18,6 @@ import { getFsUtils } from '../fsUtils'
 import { jsonPlugin } from './json'
 import { filteredResolvePlugin, resolvePlugin } from './resolve'
 import { optimizedDepsPlugin } from './optimizedDeps'
-import { esbuildPlugin } from './esbuild'
 import { importAnalysisPlugin } from './importAnalysis'
 import { cssAnalysisPlugin, cssPlugin, cssPostPlugin } from './css'
 import { assetPlugin } from './asset'
@@ -34,6 +33,7 @@ import { assetImportMetaUrlPlugin } from './assetImportMetaUrl'
 import { metadataPlugin } from './metadata'
 import { dynamicImportVarsPlugin } from './dynamicImportVars'
 import { importGlobPlugin } from './importMetaGlob'
+import { oxcPlugin } from './oxc'
 
 export async function resolvePlugins(
   config: ResolvedConfig,
@@ -114,7 +114,7 @@ export async function resolvePlugins(
     config.esbuild !== false
       ? enableNativePlugin
         ? nativeTransformPlugin()
-        : esbuildPlugin(config)
+        : oxcPlugin(config)
       : null,
     enableNativePlugin
       ? nativeJsonPlugin({
